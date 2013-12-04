4f122e96d157b2642acab749a76161e5dee0834e
KommuSoft <vanonsem.willem@gmail.com>
2013-12-04 23:01:26 +0100

    doc ContextWriteableUtils

--- Utils/ContextWriteableUtils.cs
+++ Utils/ContextWriteableUtils.cs
@@ -18,26 +18,35 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using System;
 using System.IO;
 
 namespace ZincOxide.Utils {
 
-    public static class ContextWriteableUtils {
+	public static class ContextWriteableUtils {
+		/// <summary>
+		/// Writes the content of the <paramref name="writeable"/> to the <paramref name="stream"/> with indent context.
+		/// </summary>
+		/// <param name="writeable">The writeable that should be written to the <paramref name="stream"/>.</param>
+		/// <param name="stream">The target of the <paramref name="writeable"/>.</param>
+		public static void Write (this IContextWriteable writeable, Stream stream) {
+			using (ContextStreamWriter sw = new ContextStreamWriter (stream)) {
+				writeable.Write (sw);
+			}
+		}
 
-        public static void Write (this IContextWriteable writeable, Stream stream) {
-            using (ContextStreamWriter sw = new ContextStreamWriter (stream)) {
-                writeable.Write (sw);
-            }
-        }
-
-        public static void Write (this IContextWriteable writeable, string filename, FileMode mode = FileMode.OpenOrCreate) {
-            using (FileStream fs = File.Open (filename, mode, FileAccess.Write)) {
-                writeable.Write (fs);
-            }
-        }
-
-
-    }
+		/// <summary>
+		/// Write the content of the <paramref name="writeable"/> to a file specified by a filename and mode.
+		/// </summary>
+		/// <param name="writeable">The writeable that should be written to the file.</param>
+		/// <param name="filename">The name of the output file.</param>
+		/// <param name="mode">The mode that specifies how the file should be created.</param>
+		public static void Write (this IContextWriteable writeable, string filename, FileMode mode = FileMode.OpenOrCreate) {
+			using (FileStream fs = File.Open (filename, mode, FileAccess.Write)) {
+				writeable.Write (fs);
+			}
+		}
+	}
 }
 
