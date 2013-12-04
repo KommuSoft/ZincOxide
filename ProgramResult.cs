2313edd48cac7ee5eeed4e88f74d84c5f308625d
KommuSoft <vanonsem.willem@gmail.com>
2013-12-04 17:18:59 +0100

    doc level 0

--- ProgramResult.cs
+++ ProgramResult.cs
@@ -18,18 +18,42 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using System;
 
 namespace ZincOxide {
 
-    public enum ProgramResult : byte {
-        Compiled     = 0x00,
-        Match        = 0x00,
-        Succes       = 0x00,
-        RuntimeError = 0x01,
-        NoMatch      = 0x01,
-        StaticError  = 0x02,
-        CorruptInput = 0x03
-    }
-
+	/// <summary>
+	/// An enumeration describing the different exit codes and their meaning.
+	/// </summary>
+	public enum ProgramResult : byte {
+		/// <summary>
+		/// The MiniZinc file is compiled to heuristics.
+		/// </summary>
+		Compiled = 0x00,
+		/// <summary>
+		/// The MiniZinc file and the datafile match.
+		/// </summary>
+		Match = 0x00,
+		/// <summary>
+		/// The given query or test was successfull. Or the hyper-heuristic was executed correctly.
+		/// </summary>
+		Succes = 0x00,
+		/// <summary>
+		/// There was a runtime error when executing the hyper-heuristic.
+		/// </summary>
+		RuntimeError = 0x01,
+		/// <summary>
+		/// The MiniZinc file and the data file don't match.
+		/// </summary>
+		NoMatch = 0x03,
+		/// <summary>
+		/// There was a static error. For instance the semantic of the MiniZinc file is not valid.
+		/// </summary>
+		StaticError = 0x02,
+		/// <summary>
+		/// The given input is not valid. For instance a corrupt MiniZinc file, no heuristics provided,...
+		/// </summary>
+		CorruptInput = 0x03
+	}
 }
--- ZincOxideException.cs
+++ ZincOxideException.cs
@@ -18,28 +18,56 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using System;
 using System.Runtime.Serialization;
 
 namespace ZincOxide {
 
-    public class ZincOxideException : Exception {
+	/// <summary>
+	/// The root of all exceptions that can be thrown by the ZincOxide system.
+	/// </summary>
+	public class ZincOxideException : Exception {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ZincOxide.ZincOxideException"/> class without specifying a message or reason.
+		/// </summary>
+		public ZincOxideException () : base () {
+		}
 
-        public ZincOxideException () : base() {
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ZincOxide.ZincOxideException"/> class with a message explaining the error.
+		/// </summary>
+		/// <param name="message">A message explaining the error.</param>
+		public ZincOxideException (string message) : base (message) {
+		}
 
-        public ZincOxideException (string message) : base(message) {
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ZincOxide.ZincOxideException"/> class. With a parameteric message.
+		/// </summary>
+		/// <param name="format">The format of the error message.</param>
+		/// <param name="args">Parameters that are shifted into the message.</param>
+		/// <exception cref="ArgumentNullException"><paramref name="format"/> or <paramref name="args"/> is null. </exception>
+		/// <exception cref="FormatException"><paramref name="format"/> is invalid. </exception>
+		/// <exception cref="FormatException"><paramref name="format"/> The index of a format item is less than zero, or greater than or equal to the length of the <paramref name="args"/> array.  </exception>
+		public ZincOxideException (string format, params object[] args) : base (string.Format (format, args)) {
+		}
 
-        public ZincOxideException (string format, params object[] args) : base(string.Format(format,args)) {
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ZincOxide.ZincOxideException"/> class. With serialization and streaming info.
+		/// </summary>
+		/// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
+		/// <param name="context">The <see cref="StreamingContext"/> that contains contextual information about the source or destination.</param>
+		/// <exception cref="ArgumentNullException">The <paramref name="info"/> parameter is null.</exception>
+		/// <exception cref="SerializationException">The class name is null or <see cref="Exception.HResult"/> is zero (0).</exception>
+		public ZincOxideException (SerializationInfo info, StreamingContext context) : base (info, context) {
+		}
 
-        public ZincOxideException (SerializationInfo info, StreamingContext context) : base(info,context) {
-        }
-
-        public ZincOxideException (string message, Exception innerException) : base(message,innerException) {
-        }
-
-    }
-
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ZincOxide.ZincOxideException"/> class with a message explaining the error and another Exception that caused that exception.
+		/// </summary>
+		/// <param name="message">The error message that explains the reason for the exception.</param>
+		/// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified. </param>
+		public ZincOxideException (string message, Exception innerException) : base (message, innerException) {
+		}
+	}
 }
