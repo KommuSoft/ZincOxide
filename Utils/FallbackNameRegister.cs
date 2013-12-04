4379688eabe446e2290455125c7d3986ebc5d4d8
KommuSoft <vanonsem.willem@gmail.com>
2013-12-04 23:18:39 +0100

    doc FallbackNameRegister

--- Utils/FallbackNameRegister.cs
+++ Utils/FallbackNameRegister.cs
@@ -18,47 +18,61 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using System;
 
 namespace ZincOxide.Utils {
 
-    public class FallbackNameRegister<T> : NameRegister<T>, IFallbackNameRegister<T> where T : IName {
+	/// <summary>
+	/// A name register with a fallback function that generates an object for a given name.
+	/// </summary>
+	public class FallbackNameRegister<T> : NameRegister<T>, IFallbackNameRegister<T> where T : IName {
+		/// <summary>
+		/// The function that given the name register doesn't contain a key, generates an object that is stored in the name register.
+		/// </summary>
+		/// <value>The fallback.</value>
+		public DNameRegisterFallback<T> Fallback {
+			get;
+			protected set;
+		}
 
-        private DNameRegisterFallback<T> fallback;
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ZincOxide.Utils.FallbackNameRegister{T}"/> class with a specified fallback mechanism.
+		/// </summary>
+		/// <param name="fallback">A function that generates an object when a specific name cannot be found.</param>
+		public FallbackNameRegister (DNameRegisterFallback<T> fallback) {
+			this.Fallback = fallback;
+		}
 
-        public DNameRegisterFallback<T> Fallback {
-            get {
-                return this.fallback;
-            }
-            protected set {
-                this.fallback = value;
-            }
-        }
+		/// <summary>
+		/// Checks if the name register contains the given key.
+		/// </summary>
+		/// <param name="name">The specified key.</param>
+		public override bool Contains (string name) {
+			if (!base.Contains (name)) {
+				try {
+					this.Fallback (name);
+					return true;
+				} catch (ZincOxideNameNotFoundException) {
+					return false;
+				}
+			}
+			return true;
+		}
 
-        public FallbackNameRegister (DNameRegisterFallback<T> fallback) {
-            this.Fallback = fallback;
-        }
-
-        public override bool Contains (string name) {
-            if (!base.Contains (name)) {
-                try {
-                    this.fallback (name);
-                    return true;
-                } catch (ZincOxideNameNotFoundException) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        public override T Lookup (string name) {
-            try {
-                return base.Lookup (name);
-            } catch (ZincOxideNameNotFoundException) {
-                return this.Fallback (name);
-            }
-        }
-
-    }
+		/// <summary>
+		/// Looks up the given name in the name register. If the register contains an object with the given name, the
+		/// object is returned. Otherwise the object is generated using the fallback mechanism, stored in register and
+		/// returned.
+		/// </summary>
+		/// <param name="name">The specified key to lookup.</param>
+		public override T Lookup (string name) {
+			try {
+				return base.Lookup (name);
+			} catch (ZincOxideNameNotFoundException) {
+				return this.Fallback (name);
+			}
+		}
+	}
 }
 
