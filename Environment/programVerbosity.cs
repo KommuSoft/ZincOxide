994828138484573619e2d13723d0b450c60421a9
KommuSoft <vanonsem.willem@gmail.com>
2013-12-05 22:24:51 +0100

    doc ProgramVerbosity

--- Environment/programVerbosity.cs
+++ Environment/programVerbosity.cs
@@ -18,17 +18,36 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using System;
 
 namespace ZincOxide.Environment {
 
-    [Flags]
-    public enum ProgramVerbosity : byte {
-        Remark     = 0x08,
-        Assumption = 0x04,
-        Warning    = 0x02,
-        Error      = 0x01
-    }
-
+	/// <summary>
+	/// An enumeration of levels of the verbosity of the program. Note that <see cref="FlagsAttribute"/> is enabled: one can select
+	/// multiple verbosity levels.
+	/// </summary>
+	[Flags]
+	public enum ProgramVerbosity : byte {
+		/// <summary>
+		/// Remarks is a verbosity level where simple message printed giving information about the current flow of the program.
+		/// </summary>
+		Remark = 0x08,
+		/// <summary>
+		/// Assumptions are a verbosity level where messages give information about the assumptions made by the program.
+		/// This is useful to debug or to reason why the program takes certain decisions.
+		/// </summary>
+		Assumption = 0x04,
+		/// <summary>
+		/// Warnings are a verbosity level where messages note strange aspects in the input who are not necessary wrong, but atypical.
+		/// For instance a variable is specified who is not bounded by any constraints.
+		/// </summary>
+		Warning = 0x02,
+		/// <summary>
+		/// Errors are a verbosity level where message note that certain aspects of the input can't be interpreted. For instance because
+		/// the input file doesn't follow the specific rules.
+		/// </summary>
+		Error = 0x01
+	}
 }
 
