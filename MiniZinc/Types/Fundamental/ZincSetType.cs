1cd4d8ae9d3a8974533f531db090bab4b67e2eae
KommuSoft <vanonsem.willem@gmail.com>
2013-12-06 12:02:46 +0100

    ZincSetType

--- MiniZinc/Types/Fundamental/ZincSetType.cs
+++ MiniZinc/Types/Fundamental/ZincSetType.cs
@@ -23,21 +23,88 @@ using System;
 
 namespace ZincOxide.MiniZinc.Types.Fundamental {
 
-	public class ZincSetType : IZincTF {
+	/// <summary>
+	/// A set is a collection with no duplicates.
+	/// </summary>
+	/// <remarks>
+	/// <para>The type-ins of a set's elements must be fixed. This is because current solvers are not powerful
+	/// enough to handle sets containing decision variables. Sets may contain any type and may be fixed or
+	/// unfixed. If a set is unfixed, its elements must be finite. Unless it occurs in one the following contexts:
+	/// <list type="bullet">
+	/// <item><description>The argument of a predicate, function or annotations.</description></item>
+	/// <item><description>The declaration of a variable or let local variable with an assigned value.</description></item>
+	/// </list></para>
+	/// <para>A set based type-inst expression tail has this syntax: <c>set of </c> followed by a type-instance expression.</para>
+	/// <para>The type is finite if the set elements are finite types. Otherwise, no. The domain of a set type that is a finite
+	/// type is the powerset of the domain of its element types.</para>
+	/// <para>Sets are ordered using the lexicographical ordering on the corresponding characteristics arrays. For example:
+	/// <code>
+	/// {} &lt; {2} &lt; {1,3}
+	/// </code>
+	/// Since sets have no inherent ordening themselves. The sets are first sorted.
+	/// </para>
+	/// <para>A fixed set variable must be initialized at instance-time. An unfixed set variable need not be.</para>
+	/// <para>The varification of a set is defined as: <c>par set of TI</c> to <c>var set of TI</c>
+	/// and <c>var set of TI</c> to <c>var set of TI</c>.</para>
+	/// <para>The coercion of a set is defined as: <c>par set of TI</c> to <c>par set of UI</c> and
+	/// <c>par set of TI</c> to <c>var set of UI</c> and <c>var set of TI</c> to <c>var set of UI</c> if
+	/// <c>TI</c> can be coerced to <c>UI</c>.</para>
+	/// <para>Fixed sets can be automatically coerced to arrays. This means that array acces can be used on fixed sets.
+	/// <c>S[1]</c> is the smallest element in a fixed set <c>S</c> while <c>S[card(S)]</c> is the largest.</para>
+	/// </remarks>
+	/// <example>
+	/// Some example set type-instance expressions:
+	/// <code>
+	/// set of int
+	/// var set of bool
+	/// </code>
+	/// </example>
+	public class ZincSetType : IZincType {
 		private IZincType innerType;
 
-		public ZincSetType (IZincTF innerType) {
-			this.innerType = innerType;
+		/// <summary>
+		/// The element's type of the set.
+		/// </summary>
+		/// <value>The element's type of the set.</value>
+		/// <exception cref="ArgumentNullException">If the given argument is <see langword="null"/>.</exception>
+		public IZincType InnerType {
+			get {
+				return this.innerType;
+			}
+			protected set {
+				if (value == null) {
+					throw new ArgumentNullException ("The inner type of a set must be effective.");
+				}
+			}
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ZincOxide.MiniZinc.Types.Fundamental.ZincSetType"/> class with
+		/// a given element's type.
+		/// </summary>
+		/// <param name="elementType">The element's type of the set.</param>
+		/// <exception cref="ArgumentNullException">If the given <paramref name="elementType"/> is <see langword="null"/>.</exception>
+		public ZincSetType (IZincType elementType) {
+			this.InnerType = elementType;
 		}
 
 		public override string ToString () {
-			return string.Format ("set [{0}]", this.innerType);
+			return string.Format ("set of [{0}]", this.innerType);
 		}
 
 		#region IGenericEquals implementation
 
-		public bool GenericEquals (IZincTF other) {
-			throw new NotImplementedException ();
+		/// <summary>
+		/// Checks if this zinc type is equal to the given zinc type.
+		/// </summary>
+		/// <returns><see langword="true"/>, if the types where equal, <see langword="false"/> otherwise.</returns>
+		/// <param name="other">The <see cref="IZincType"/> to match this type against.</param>
+		public bool GenericEquals (IZincType other) {
+			if (other != null && other is ZincSetType) {
+				ZincSetType zst = (ZincSetType)other;
+				return this.innerType.GenericEquals (zst.innerType);
+			}
+			return false;
 		}
 
 		#endregion
