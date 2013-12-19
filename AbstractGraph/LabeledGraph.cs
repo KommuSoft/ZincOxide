dd6fdd2b99ae267b829a2c9f4a597460f953f5d4
KommuSoft <vanonsem.willem@gmail.com>
2013-12-19 19:13:10 +0100

    bugfic

--- AbstractGraph/LabeledGraph.cs
+++ AbstractGraph/LabeledGraph.cs
@@ -3,44 +3,61 @@ using System.Collections.Generic;
 
 namespace ZincOxide.AbstractGraph {
 
+	/// <summary>
+	/// A representation of a labeled graph as described in "Arend Rensink (2005), Abstract Graph Transformation".
+	/// </summary>
 	public class LabeledGraph<TNode,TLabel> {
 		private readonly Dictionary<TNode,Node> nodes = new Dictionary<TNode, Node> ();
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ZincOxide.AbstractGraph.LabeledGraph`2"/> class with a given set of nodes and edges.
+		/// </summary>
+		/// <param name="nodes">The set of nodes.</param>
+		/// <param name="edges">The set of edges.</param>
 		public LabeledGraph (IEnumerable<TNode> nodes, params Tuple<TNode,TLabel,TNode>[] edges) : this (nodes, (IEnumerable<Tuple<TNode,TLabel,TNode>>)edges) {
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ZincOxide.AbstractGraph.LabeledGraph`2"/> class with a given set of nodes and edges.
+		/// </summary>
+		/// <param name="nodes">The set of nodes.</param>
+		/// <param name="edges">The set of edges.</param>
 		public LabeledGraph (IEnumerable<TNode> nodes, IEnumerable<Tuple<TNode,TLabel,TNode>> edges) {
 			foreach (TNode node in nodes) {
 				this.nodes.Add (node, new Node (node));
 			}
 			foreach (Tuple<TNode,TLabel,TNode> edge in edges) {
-				Node nod;
-				TNode frm = edge.Item1;
+				Node nod, noe;
+				TNode frm = edge.Item1, ton = edge.Item3;
 				if (!this.nodes.TryGetValue (frm, out nod)) {
 					nod = new Node (frm);
 					this.nodes.Add (frm, nod);
 				}
-				nod.AddEdge (edge.Item2, edge.Item3);
+				if (!this.nodes.TryGetValue (ton, out noe)) {
+					noe = new Node (ton);
+					this.nodes.Add (ton, noe);
+				}
+				nod.AddEdge (edge.Item2, noe);
 			}
 		}
 
-		public class Node : TagBase<TNode> {
-			private readonly Dictionary<TLabel,TNode> edges = new Dictionary<TLabel, TNode> ();
+		private class Node : TagBase<TNode> {
+			private readonly Dictionary<TLabel,Node> edges = new Dictionary<TLabel, Node> ();
 
-			public Node (TNode node, IEnumerable<Tuple<TLabel,TNode>> edges) : base (node) {
-				foreach (Tuple<TLabel,TNode> edge in edges) {
+			public Node (TNode node, IEnumerable<Tuple<TLabel,Node>> edges) : base (node) {
+				foreach (Tuple<TLabel,Node> edge in edges) {
 					this.AddEdge (edge);
 				}
 			}
 
-			public Node (TNode node, params Tuple<TLabel,TNode>[] edges) : this (node, (IEnumerable<Tuple<TLabel,TNode>>)edges) {
+			public Node (TNode node, params Tuple<TLabel,Node>[] edges) : this (node, (IEnumerable<Tuple<TLabel,Node>>)edges) {
 			}
 
-			public void AddEdge (Tuple<TLabel,TNode> edge) {
+			public void AddEdge (Tuple<TLabel,Node> edge) {
 				this.AddEdge (edge.Item1, edge.Item2);
 			}
 
-			public void AddEdge (TLabel label, TNode node) {
+			public void AddEdge (TLabel label, Node node) {
 				this.edges.Add (label, node);
 			}
 		}
