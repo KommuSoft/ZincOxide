cdc31b6f3c6227454fee5baa5af4722710b213b3
KommuSoft <vanonsem.willem@gmail.com>
2014-04-14 03:13:36 +0200

    NumNumBox

--- MiniZinc/Boxes/ZincNumNumBoxBase.cs
+++ MiniZinc/Boxes/ZincNumNumBoxBase.cs
@@ -24,38 +24,85 @@ using ZincOxide.Utils;
 
 namespace ZincOxide.MiniZinc.Boxes {
 
-    public class ZincNumNumBoxBase : ZincNumBoxBase, IZincNumNumBox {
+	/// <summary>
+	/// An implementation of the <see cref="IZincNumNumBox"/> interface. A <see cref="ZincBoxBase"/> that contains
+	/// two <see cref="IZincNumExp"/> instances.
+	/// </summary>
+	public class ZincNumNumBoxBase : ZincNumBoxBase, IZincNumNumBox {
 
-        private IZincNumExp numExp2;
+		private IZincNumExp numExp2;
 
         #region IZincNumNumBox implementation
-        public IZincNumExp NumericExpression2 {
-            get {
-                return this.numExp2;
-            }
-            protected set {
-                this.numExp2 = value;
-            }
-        }
+		/// <summary>
+		/// Gets the second <see cref="IZincNumExp"/> stored in the <see cref="IZincNumNumBox"/>.
+		/// </summary>
+		/// <value>
+		/// The second <see cref="IZincNumExp"/> stored in the <see cref="IZincNumNumBox"/>.
+		/// </value>
+		public IZincNumExp NumericExpression2 {
+			get {
+				return this.numExp2;
+			}
+			protected set {
+				this.numExp2 = value;
+			}
+		}
         #endregion
 
-        protected ZincNumNumBoxBase (IZincNumExp numericExpression, IZincNumExp numericExpression2) : base(numericExpression) {
-            this.NumericExpression2 = numericExpression2;
-        }
+		#region Constructors
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ZincOxide.MiniZinc.Boxes.ZincNumNumBoxBase"/> class with
+		/// two given initial <see cref="IZincNumExp"/> instances.
+		/// </summary>
+		/// <param name='numericExpression'>
+		/// The first <see cref="IZincNumExp"/> instance.
+		/// </param>
+		/// <param name='numericExpression2'>
+		/// The second <see cref="IZincNumExp"/> instance.
+		/// </param>
+		protected ZincNumNumBoxBase (IZincNumExp numericExpression, IZincNumExp numericExpression2) : base(numericExpression) {
+			this.NumericExpression2 = numericExpression2;
+		}
+		#endregion
 
-        public override IEnumerable<ZincIdent> InvolvedIdents () {
-            return EnumerableUtils.Append (this.NumericExpression.InvolvedIdents (), this.NumericExpression2.InvolvedIdents ());
-        }
+        #region IZincIdentContainer implementation
+		/// <summary>
+		/// Returns a <see cref="T:System.Collections.Generic.IEnumerable`1"/> containing the involved
+		/// <see cref="IZincIdent"/> instances of the container.
+		/// </summary>
+		/// <returns>
+		/// A <see cref="T:System.Collections.Generic.IEnumerable`1"/> containing the involved
+		/// <see cref="IZincIdent"/> instances of the container.
+		/// </returns>
+		public override IEnumerable<IZincIdent> InvolvedIdents () {
+			return EnumerableUtils.Append (this.NumericExpression.InvolvedIdents (), this.NumericExpression2.InvolvedIdents ());
+		}
+        #endregion
 
-        public override IZincIdentReplaceContainer Replace (IDictionary<ZincIdent, ZincIdent> identMap) {
-            this.NumericExpression2 = this.NumericExpression2.Replace (identMap) as IZincNumExp;
-            return base.Replace (identMap);
-        }
+        #region IZincIdentReplaceContainer implementation
+		/// <summary>
+		/// Replaces all the instances stored in the given <see cref="System.Collections.Generic.IDictionary`1"/>
+		/// stored as keys to the corresponding values and returns this instance.
+		/// </summary>
+		/// <returns>
+		/// This instance, for cascading purposes.
+		/// </returns>
+		public override IZincIdentReplaceContainer Replace (IDictionary<IZincIdent, IZincIdent> identMap) {
+			this.NumericExpression2 = this.NumericExpression2.Replace (identMap) as IZincNumExp;
+			return base.Replace (identMap);
+		}
+        #endregion
 
-        public override IEnumerable<IZincElement> Children () {
-            return EnumerableUtils.Append (this.NumericExpression2, base.Children ());
-        }
+		#region IComposition implementation
+		/// <summary>
+		/// Gets a list of involved <see cref="IZincElement"/> instances that are the children of
+		/// this <see cref="IZincElement"/>.
+		/// </summary>
+		public override IEnumerable<IZincElement> Children () {
+			return EnumerableUtils.Append (this.NumericExpression2, base.Children ());
+		}
+		#endregion
 
-    }
+	}
+
 }
-
