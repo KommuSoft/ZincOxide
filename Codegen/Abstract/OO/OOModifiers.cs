5abfce790191346ddade3f55580773131a0879cb
KommuSoft <vanonsem.willem@gmail.com>
2014-08-28 08:23:17 +0200

    modifier masks

--- Codegen/Abstract/OO/OOModifiers.cs
+++ Codegen/Abstract/OO/OOModifiers.cs
@@ -29,22 +29,74 @@ namespace ZincOxide.Codegen.Abstract.OO {
 	public enum OOModifiers : ulong {
 
 		#region Access
+		/// <summary>
+		/// A mask to filter out access out of a <see cref="OOModifiers"/>.
+		/// </summary>
+		AccessMask = 0xff0000,
+		/// <summary>
+		/// The member is publicly available.
+		/// </summary>
 		Public = 0x080000,
+		/// <summary>
+		/// The member is protected: only deriving types can access.
+		/// </summary>
 		Protected = 0x040000,
+		/// <summary>
+		/// The member is only available to the assembly.
+		/// </summary>
 		Internal = 0x020000,
+		/// <summary>
+		/// The member is privately.
+		/// </summary>
 		Private = 0x010000,
 		#endregion
 		#region Specification
+		/// <summary>
+		/// A mask to filter out specifications out of a <see cref="OOModifiers"/>.
+		/// </summary>
+		SpecificationMask = 0xff00,
+		/// <summary>
+		/// The member is read-only: the value can't be override.
+		/// </summary>
 		Readonly = 0x0800,
+		/// <summary>
+		/// The member serves as an input value.
+		/// </summary>
 		In = 0x0400,
+		/// <summary>
+		/// The member serves as an output value.
+		/// </summary>
 		Out = 0x0200,
+		/// <summary>
+		/// The member is communicated by reference.
+		/// </summary>
 		Ref = 0x0100,
 		#endregion
 		#region Implementation
+		/// <summary>
+		/// A mask to filter out implementation issues out of a <see cref="OOModifiers"/>.
+		/// </summary>
+		ImplementationMask = 0xff,
+		/// <summary>
+		/// The member is abstract: no implementation is provided.
+		/// </summary>
 		Abstract = 0x10,
+		/// <summary>
+		/// The member is virtual: children members can override the member.
+		/// </summary>
+		/// <seealso cref="Override"/>
 		Virtual = 0x08,
+		/// <summary>
+		/// The member is static, one does not need an instance to access it.
+		/// </summary>
 		Static = 0x04,
+		/// <summary>
+		/// The member is final: no "child" members are allowed.
+		/// </summary>
 		Final = 0x02,
+		/// <summary>
+		/// The member is an override of a "parent" member.
+		/// </summary>
 		Override = 0x01
 		#endregion
 	}
