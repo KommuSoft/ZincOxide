5a7fcf44a12a9322cc7c3f64a26a7c543183be1a
KommuSoft <vanonsem.willem@gmail.com>
2014-09-02 23:26:41 +0200

    command sequence filtering

--- Codegen/Abstract/Imperative/CommandSequenceBase.cs
+++ Codegen/Abstract/Imperative/CommandSequenceBase.cs
@@ -19,7 +19,9 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 
 namespace ZincOxide.Codegen.Abstract.Imperative {
 
@@ -53,9 +55,57 @@ namespace ZincOxide.Codegen.Abstract.Imperative {
 		/// <remarks>
 		/// <para>The commands are flatten out: if one of the commands is a <see cref="ICommandSequence"/>, the commands
 		/// are unrolled and stored as single units in this <see cref="CommandSequenceBase"/>.</para>
+		/// <para><c>null</c> values in the given <paramref name="commands"/> list will not be added to this <see cref="CommandSequenceBase"/>.</para>
 		/// </remarks>
 		public CommandSequenceBase (IEnumerable<ICommand> commands) {
-
+			if (commands != null) {
+				this.CommandList = new LinkedList<ICommand> (commands.SelectMany (deployCommands));
+			}
+			Contract.Ensures (this.CommandList != null);
+		}
+		#endregion
+		#region Private methods
+		/// <summary>
+		/// A method that deploys the given <see cref="ICommand"/>.
+		/// </summary>
+		/// <returns>A list of <see cref="ICommand"/> instances that are equivalent to the given <see cref="ICommand"/> .</returns>
+		/// <param name="command">The given <see cref="ICommand"/> to deploy.</param>
+		/// <remarks>
+		/// <para>By deploying, all <c>null</c> references are removed, if the command is a <see cref="ICommandSequence"/>
+		/// the list of <see cref="ICommand"/> instances is returned (this is performed recursively).</para>
+		/// </remarks>
+		private static IEnumerable<ICommand> deployCommands (ICommand command) {
+			if (command != null) {
+				ICommandSequence ics = command as ICommandSequence;
+				if (ics != null) {
+					foreach (ICommand cmd in ics.SelectMany (deployCommands)) {
+						yield return cmd;
+					}
+				} else {
+					yield return ics;
+				}
+			}
+		}
+		#endregion
+		#region IEnumerable implementation
+		/// <summary>
+		/// Enumerate all the commands that are stored in this <see cref="CommandSequenceBase"/>.
+		/// </summary>
+		/// <returns>A <see cref="T:IEnumerator`1"/> that emits all the <see cref="ICommand"/> instances stored in this <see cref="CommandSequenceBase"/>.</returns>
+		/// <remarks>
+		/// <para>It is guaranteed that no <c>null</c> pointers will be enumerated.</para>
+		/// </remarks>
+		public IEnumerator<ICommand> GetEnumerator () {
+			return this.CommandList.GetEnumerator ();
+		}
+		#endregion
+		#region IEnumerable implementation
+		/// <summary>
+		/// Enumerate all the items, accessed through the <see cref="T:System.Collections.IEnumerable"/> interface.
+		/// </summary>
+		/// <returns>All the items enumerated by <see cref="M:GetEnumerator"/>, but ungeneric.</returns>
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator () {
+			return this.GetEnumerator ();
 		}
 		#endregion
 	}
